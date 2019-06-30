52fefdffdad342df5ebe09618bb837e329a7e22d
Steven Dao <stevedao@gmail.com>
2019-06-30 00:56:45 -0700

    water for level1

---
+++ Assets/FriendlyGameJam6/Scripts/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Transform xf = GetComponent<Transform>();
+        Vector3 euler = xf.eulerAngles;
+        euler.y = Time.time;
+        xf.eulerAngles = euler;
+    }
+}
